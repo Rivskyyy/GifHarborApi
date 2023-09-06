e465ca295d5330d02c1675bb159475e83f78c669
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-06 08:55:01 +0300

    added ApiGIf

---
+++ app/src/main/java/com/rivskyinc/gifharborapi/data/repository/ApiGIf.kt
@@ -0,0 +1,17 @@
+package com.rivskyinc.gifharborapi.data.repository
+
+import com.rivskyInc.gifharbor.Utils.API_KEY
+import com.rivskyInc.gifharbor.domain.entity_api.Gif
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiGIf {
+
+        @GET("gifs/trending?api_key=$API_KEY") //  <-  put your API_KEY
+        suspend fun gifResponse(
+                @Query("id")
+                 gif_id: String?
+        ) : Response<Gif>
+
+}
