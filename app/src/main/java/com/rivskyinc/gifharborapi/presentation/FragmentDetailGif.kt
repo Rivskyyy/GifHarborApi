4bdb036286a81872f6e5c653d2d7b41b954161d5
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-06 15:50:53 +0300

    added Toast if no internet

--- app/src/main/java/com/rivskyinc/gifharborapi/presentation/FragmentDetailGif.kt
+++ app/src/main/java/com/rivskyinc/gifharborapi/presentation/FragmentDetailGif.kt
@@ -1,25 +1,25 @@
 package com.rivskyinc.gifharborapi.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.rivskyinc.gifharborapi.R
 import com.rivskyinc.gifharborapi.databinding.FragmentDetailGifBinding
 
 
 class FragmentDetailGif : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var currentUrl: String? = null
 
+    private var currentUrl: String? = null
 
     private lateinit var binding: FragmentDetailGifBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             currentUrl = it.getString(URL)
+            Log.d("Fragment", it.toString())
         }
     }
 
@@ -28,22 +28,33 @@ class FragmentDetailGif : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        binding = FragmentDetailGifBinding.inflate(layoutInflater, container, false )
+        binding = FragmentDetailGifBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        Glide.with(this@FragmentDetailGif).load(currentUrl).into(binding.gifIvFragmentDetail)
+        binding.onBackButton.setOnClickListener {
+            activity?.onBackPressed()
+        }
+        setGif()
+    }
+
+    private fun setGif() {
+
+        activity?.let {
+            Glide.with(it.applicationContext).load(currentUrl).into(binding.gifIvFragmentDetail)
+        }
+        binding.progressBar.visibility = View.GONE
     }
 
     companion object {
 
         const val URL = "url"
+
         @JvmStatic
-        fun newInstance(url: String,)
-        =
+        fun newInstance(url: String) =
             FragmentDetailGif().apply {
                 arguments = Bundle().apply {
                     putString(URL, url)
--- app/src/main/java/com/rivskyinc/gifharborapi/presentation/MainActivity.kt
+++ app/src/main/java/com/rivskyinc/gifharborapi/presentation/MainActivity.kt
@@ -1,59 +1,80 @@
 package com.rivskyinc.gifharborapi.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.GridLayoutManager
 import com.rivskyinc.gifharborapi.R
 import com.rivskyinc.gifharborapi.databinding.ActivityMainBinding
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-@Inject
-lateinit var viewModelFactory: MainViewModelFactory
+    @Inject
+    lateinit var viewModelFactory: MainViewModelFactory
 
-private val viewModel: MainViewModel by lazy {
-    ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
-}
-
-private val component by lazy {
-    (this.application as GifApplication).component
-}
-
-override fun onCreate(savedInstanceState: Bundle?) {
-    component.inject(this)
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
-    binding.progressBar.visibility = View.VISIBLE
-
-     val adapter = MyListAdapter(this) { Data ->
-
-            FragmentDetailGif.newInstance(Data.images.original.url)
-            launchFragment(FragmentDetailGif())
-     }
-
-    binding.progressBar.visibility = View.GONE
-    viewModel.gifsListLD.observe(this){
-        adapter.submitList(it.data)
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
     }
 
-}
+    private lateinit var myAdapter: MyListAdapter
 
-private fun setupRecyclerView() {
-//        binding.recyclerViewMain.apply {
-//            myListAdapter = ListAdapter(this, )
-//            adapter = myAdapter
-//            layoutManager = GridLayoutManager(this.context, 1)
-//
+    private val component by lazy {
+        (this.application as GifApplication).component
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        component.inject(this)
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupRecyclerView()
+        binding.progressBar.visibility = View.VISIBLE
+        myAdapter.onClickListener = {
+            val detailFragment = FragmentDetailGif.newInstance(it.images.original.url)
+            val fragmentManager = supportFragmentManager
+            fragmentManager.beginTransaction()
+                .replace(
+                    R.id.activity_constrait,
+                    detailFragment
+                )
+                .addToBackStack(null)
+                .commit()
         }
+
+
+
+        binding.progressBar.visibility = View.GONE
+
+        viewModel.gifsListLD.observe(this) {
+            if ( it!= null ){
+                myAdapter.submitList(it.data)
+            } else {
+                binding.progressBar.visibility = View.VISIBLE
+                Toast.makeText(this, "No Internet connection", Toast.LENGTH_LONG).show()
+            }
+
+
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.recyclerViewMain.apply {
+            myAdapter = MyListAdapter()
+            adapter = myAdapter
+            layoutManager = GridLayoutManager(context, 1)
+
+        }
+    }
+
+
     private fun launchFragment(fragment: Fragment) {
-             supportFragmentManager.beginTransaction()
+        supportFragmentManager.beginTransaction()
             .replace(R.id.activity_constrait, fragment)
             .addToBackStack(null)
             .commit()
