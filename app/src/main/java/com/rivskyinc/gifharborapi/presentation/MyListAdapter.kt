fffa779ed75f8d12dfa7352c0f2daa8ed94fc34f
Taras Bandrivskyi <taras.bandrivskyi@gmail.com>
2023-09-06 15:51:54 +0300

    MyListAdapter.kt - onBindViewHolder modified

--- app/src/main/java/com/rivskyinc/gifharborapi/presentation/MyListAdapter.kt
+++ app/src/main/java/com/rivskyinc/gifharborapi/presentation/MyListAdapter.kt
@@ -1,30 +1,16 @@
 package com.rivskyinc.gifharborapi.presentation
 
-import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.AdapterView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.rivskyinc.gifharborapi.databinding.ItemLayoutBinding
 import com.rivskyinc.gifharborapi.domain.pojo.data.Data
 
-class MyListAdapter(private val context: Context,
-                    private val listener: (Data) -> Unit) : ListAdapter<Data, MyListAdapter.GifsViewHolder>(GifItemDiffCallback()) {
+class MyListAdapter : ListAdapter<Data, MyListAdapter.GifsViewHolder>(GifItemDiffCallback()) {
 
     var onClickListener: ((Data) -> Unit)? = null
-
-    lateinit var mListener : AdapterView.OnItemClickListener
-
-    interface onItemClickListener{
-        fun onItemClick(position: Int)
-    }
-
-    fun setOnItemClickListener(listener : AdapterView.OnItemClickListener){
-        mListener = listener
-    }
 
     class GifsViewHolder(val binding: ItemLayoutBinding) : RecyclerView.ViewHolder(binding.root)
 
@@ -39,15 +25,18 @@ class MyListAdapter(private val context: Context,
     }
 
     override fun onBindViewHolder(holder: GifsViewHolder, position: Int) {
+
         val listOfGifs = getItem(position)
         holder.binding.tvTitle.text = listOfGifs.title
-        Glide.with(context).load(listOfGifs.images.fixed_height_small.url)
+        Glide.with(holder.itemView).load(listOfGifs.images.fixed_height_small.url)
             .into(holder.binding.gifIv)
+
 
         holder.itemView.setOnClickListener {
             onClickListener?.invoke(listOfGifs)
-            Log.d("Adapter", it.id.toString())
         }
-
     }
+
+
 }
+
